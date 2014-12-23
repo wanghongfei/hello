4aeebe67a69fa80863df4e5d653a62bb2370f840
wanghongfei <brucewhf@gmail.com>
2014-12-23 11:18:57 +0800

    add web-security lib to dependency

--- hello-common/src/main/java/cn/fh/hello/common/exception/unchecked/InvalidXmlFileException.java
+++
@@ -1,7 +0,0 @@
-package cn.fh.hello.common.exception.unchecked;
-
-public class InvalidXmlFileException extends RuntimeException {
-	public InvalidXmlFileException(String msg) {
-		super(msg);
-	}
-}
